8603c78f04668fd96e384f80c52d6404656dcabd
Suuper <SuuperW@gmail.com>
2017-10-30 16:44:33 -0500

    Refactor of code to use a list of commands instead of a chain of else-ifs. Also brought in a modified version of the command parser in Level_Generator_ConsoleUI. Also changed playing text to "with RNG".

--- LevelGenBot/GenBot.cs
+++ LevelGenBot/GenBot.cs
@@ -33,6 +33,8 @@ namespace LevelGenBot
 			bot_token = json["bot_token"].ToString();
 			pr2_username = json["pr2_username"].ToString();
 			pr2_token = json["pr2_token"].ToString();
+
+			InitializeBotCommandsList();
 		}
 
 		public event Action Connected;
@@ -44,7 +46,7 @@ namespace LevelGenBot
 
 			socketClient.MessageReceived += SocketClient_MessageReceived;
 
-			await socketClient.SetGameAsync("with .NET");
+			await socketClient.SetGameAsync("with RNG");
 		}
 
 		public async Task Disconnect()
@@ -86,38 +88,91 @@ namespace LevelGenBot
 			{
 				if (msg.MentionedUsers.FirstOrDefault(u => u.Id == BotID) != null)
 				{
-					Console.WriteLine("Received message: " + msg.Content);
+					Console.WriteLine("Received message from " + msg.Author.Username + ": " + msg.Content);
+					Task task;
+
+					// Ensure command is in proper format (@bot args)
 					string[] words = msg.Content.Split(' ');
 					MentionUtils.TryParseUser(words[0], out ulong mentionedID);
 					if (mentionedID != MentionUtils.ParseUser(socketClient.CurrentUser.Mention) || words.Length < 2)
-						await SendFormatHelpMessage(msg.Channel, msg.Author);
-					else if (words[1].ToLower() == "getsettings")
-						await SendSettingsListMessage(msg.Author);
-					else if (words[1].ToLower() == "generate")
-					{
-						if (words.Length < 3)
-							await SendFormatHelpMessage(msg.Channel, msg.Author);
-						else
-						{
-							string settingsName = msg.Content.Substring(words[0].Length + words[1].Length + 2);
-							string message = GenerateLevel(settingsName);
-							await msg.Channel.SendMessageAsync(msg.Author.Mention +
-								", I got this message from pr2hub.com:\n`" + message + "`");
-
-						}
-					}
+						task = SendHelpMessage(msg, null);
 					else
-						await SendFormatHelpMessage(msg.Channel, msg.Author);
+					{// It is.
+						string[] args = ParseCommand(msg.Content);
+						if (botCommands.ContainsKey(args[0]))
+							task = botCommands[args[0]](msg, args);
+						else
+							task = SendHelpMessage(msg, null);
+					}
+
+					await task;
+					if (task.Exception != null)
+						throw task.Exception;
 				}
 			}
 		}
-		private async Task SendFormatHelpMessage(ISocketMessageChannel channel, SocketUser user)
+		private string[] ParseCommand(string msg)
 		{
-			await channel.SendMessageAsync(user.Mention + ", to generate a level, please use the following format:\n" +
-				"```@me generate [name of settings to use]```\n" +
-				"To see a list of available settings, say `@me getsettings`.");
+			// This method assumes msg has already been verified to be a command.
+			// Thus, there will be a > char marking the end of the bot mention.
+			int index = msg.IndexOf('>') + 1;
+			List<string> list = new List<string>();
+
+			do
+			{
+				while (msg.Length < index && char.IsWhiteSpace(msg[index]))
+					index++;
+
+				int quote = msg.IndexOf('"', index + 1);
+				int space = msg.IndexOf(' ', index + 1);
+				int newIndex = 0;
+				if (quote != -1 && (quote < space || space == -1))
+				{
+					newIndex = msg.IndexOf('"', quote + 1);
+					list.Add(msg.Substring(quote + 1, newIndex - quote - 1));
+					newIndex++;
+				}
+				else
+				{
+					if (space == -1)
+						newIndex = msg.Length;
+					else
+						newIndex = space;
+					list.Add(msg.Substring(index + 1, newIndex - index - 1));
+				}
+
+				index = newIndex;
+			} while (index != msg.Length);
+
+			return list.ToArray();
 		}
-		private async Task SendSettingsListMessage(SocketUser user)
+
+		#region "Bot Commands"
+		private delegate Task BotCommand(SocketMessage msg, params string[] args);
+		private SortedList<string, BotCommand> botCommands;
+		private void InitializeBotCommandsList()
+		{
+			botCommands = new SortedList<string, BotCommand>();
+			botCommands.Add("help", SendHelpMessage);
+			botCommands.Add("getsettings", SendSettingsListMessage);
+			botCommands.Add("generate", GenerateLevel);
+		}
+
+		private async Task SendHelpMessage(SocketMessage msg, params string[] args)
+		{
+			StringBuilder availableCommands = new StringBuilder();
+			foreach (KeyValuePair<string, BotCommand> kvp in botCommands)
+				availableCommands.Append("\n" + kvp.Key); // \n first compensates for a bug in Discord
+
+			await msg.Author.SendMessageAsync(msg.Author.Mention +
+				", to use this bot send a message with the format `@me command [command arguments]`.\n" +
+				"If a command or argument contains a space, surround it with quotation marks.\n" +
+				"Example: `@·Level Gen Bot generate \"long race\"`\n" +
+				"List of available commands: ```" + availableCommands.ToString() + "```");
+
+			Console.WriteLine("Sent help to " + msg.Author.Username + ".");
+		}
+		private async Task SendSettingsListMessage(SocketMessage msg, params string[] args)
 		{
 			IEnumerable<string> filesList = Directory.EnumerateFiles("GenSettings", "*", SearchOption.AllDirectories);
 			StringBuilder settingsList = new StringBuilder("Here is a list of all available settings:\n```");
@@ -127,17 +182,53 @@ namespace LevelGenBot
 			}
 			settingsList.Append("```");
 
-			await user.SendMessageAsync(settingsList.ToString());
+			await msg.Author.SendMessageAsync(settingsList.ToString());
+			Console.WriteLine("Sent settings list to " + msg.Author.Username + ".");
 		}
-		private string GenerateLevel(string settingName)
+		private async Task GenerateLevel(SocketMessage msg, params string[] args)
 		{
+			if (args.Length < 2)
+			{
+				await SendGenerateHelpMessage(msg);
+				return;
+			}
+
+			string settingsName = args[1];
+
 			GenerationManager generationManager = new GenerationManager();
 			generationManager.username = pr2_username;
 			generationManager.login_token = pr2_token;
 
-			generationManager.LoadSettings(Path.Combine("GenSettings", settingName));
+			if (generationManager.LoadSettings(Path.Combine("GenSettings", args[1])) == null)
+			{
+				await msg.Channel.SendMessageAsync(msg.Author.Mention + ", " +
+					"`" + args[1] + "` is not a recognized setting.");
+				return;
+			}
+
+			RestUserMessage generatingMessage = await msg.Channel.SendMessageAsync(msg.Author.Mention +
+				", I am generating and uploading your level...");
+
 			generationManager.generator.GenerateMap();
-			return generationManager.UploadLevel();
+			string response = await generationManager.UploadLevel();
+
+			generatingMessage.DeleteAsync();
+			await msg.Channel.SendMessageAsync(msg.Author.Mention +
+			  ", I got this message from pr2hub.com:\n`" + response + "`");
+			Console.WriteLine("Uploaded: " + response);
+
 		}
+		private async Task SendGenerateHelpMessage(SocketMessage msg)
+		{
+			await msg.Channel.SendMessageAsync(msg.Author.Mention +
+				", to generate a level, please use the following format:\n" +
+				"```@me generate [name of settings to use]```\n" +
+				"To see a list of available settings, say `@me getsettings`.");
+
+			Console.WriteLine("Sent generate help message to " + msg.Author.Username + ".");
+		}
+
+		#endregion
+
 	}
 }
