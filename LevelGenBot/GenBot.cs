50f63cc282ec7ccf16b035b4470b7077c6a4889a
Suuper <SuuperW@gmail.com>
2017-10-30 11:57:36 -0500

    Created class GenBot.

---
+++ LevelGenBot/GenBot.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Linq;
+
+using Discord;
+using Discord.Rest;
+using Discord.WebSocket;
+
+namespace LevelGenBot
+{
+	class GenBot
+	{
+		string botToken;
+
+		DiscordRestClient restClient;
+		DiscordSocketClient socketClient;
+
+		ulong BotID { get => socketClient.CurrentUser.Id; }
+		public bool isConnected = false;
+
+		public GenBot()
+		{
+			botToken = System.IO.File.ReadAllText("token.txt");
+		}
+
+		public event Action Connected;
+		public event Action Disconnected;
+		public async Task ConnectAndStart()
+		{
+			restClient = await ConnectRestClient();
+			socketClient = await ConnectSocketClient();
+
+			socketClient.MessageReceived += SocketClient_MessageReceived;
+
+			socketClient.SetGameAsync("with .NET");
+		}
+
+		public async Task Disconnect()
+		{
+			socketClient.Disconnected += (e) =>
+			{
+				isConnected = false;
+				Disconnected?.Invoke();
+				return null;
+			};
+			await socketClient.SetGameAsync("I'm done.");
+			await restClient.LogoutAsync();
+			await socketClient.SetStatusAsync(UserStatus.Offline);
+			await socketClient.StopAsync();
+			await socketClient.LogoutAsync();
+		}
+
+		async Task<DiscordRestClient> ConnectRestClient()
+		{
+			DiscordRestClient ret = new DiscordRestClient(new DiscordRestConfig());
+			await ret.LoginAsync(TokenType.Bot, botToken);
+
+			return ret;
+		}
+		async Task<DiscordSocketClient> ConnectSocketClient()
+		{
+			DiscordSocketClient client = new DiscordSocketClient();
+			client.Ready += () => { isConnected = true; Connected?.Invoke(); return null; };
+			await client.LoginAsync(TokenType.Bot, botToken);
+			await client.StartAsync();
+
+			return client;
+		}
+
+
+		private Task SocketClient_MessageReceived(SocketMessage arg)
+		{
+			if (arg.Author.Id != BotID)
+			{
+				if (arg.MentionedUsers.FirstOrDefault(u => u.Id == BotID) != null)
+					arg.Channel.SendMessageAsync("I don't do much yet.");
+			}
+
+			return null;
+
+		}
+	}
+}
--- LevelGenBot/Program.cs
+++ LevelGenBot/Program.cs
@@ -9,67 +9,35 @@ using Discord.WebSocket;
 
 namespace LevelGenBot
 {
-    class Program
-    {
-		static string botToken;
-		static ulong BotID { get => socketClient.CurrentUser.Id; }
+	class Program
+	{
+		static GenBot bot;
 
-		static DiscordRestClient restClient;
-		static DiscordSocketClient socketClient;
+		static void Main(string[] args)
+		{
+			bot = new GenBot();
+			bot.Connected += Connected;
+			bot.Disconnected += Disconnected;
+			bot.ConnectAndStart().Wait();
 
-        static void Main(string[] args)
-        {
-			botToken = System.IO.File.ReadAllText("token.txt");
-
-			Console.WriteLine("Creating connections...");
-			restClient = ConnectRestClient().Result;
-			socketClient = ConnectSocketClient().Result;
-
-			socketClient.SetGameAsync("with .NET");		
-
-			socketClient.MessageReceived += SocketClient_MessageReceived;
-
-			Console.WriteLine("Setup complete.");
 			string userInput = "";
 			while (userInput != "e")
 			{
 				userInput = Console.ReadLine();
 			}
 
-			Console.WriteLine("Logging out...");
-			restClient.LogoutAsync().Wait();
-			socketClient.SetStatusAsync(UserStatus.Offline).Wait();
-			socketClient.StopAsync().Wait();
-			socketClient.LogoutAsync().Wait();
-  			Console.WriteLine("Logged out.");
-      }
-
-		private static Task SocketClient_MessageReceived(SocketMessage arg)
-		{
-			if (arg.Author.Id != BotID)
-			{
-				if (arg.MentionedUsers.FirstOrDefault(u => u.Id == BotID) != null)
-					arg.Channel.SendMessageAsync("I don't do much yet.");
-			}
-
-			return null;
+			bot.Disconnect().Wait();
+			while (bot.isConnected)
+				Task.Delay(250);
 		}
 
-		static async Task<DiscordRestClient> ConnectRestClient()
+		static void Connected()
 		{
-			DiscordRestClient ret = new DiscordRestClient(new DiscordRestConfig());
-			await ret.LoginAsync(TokenType.Bot, botToken);
-
-			return ret;
+			Console.WriteLine("Connected.");
 		}
-
-		static async Task<DiscordSocketClient> ConnectSocketClient()
+		static void Disconnected()
 		{
-			DiscordSocketClient client = new DiscordSocketClient();
-			await client.LoginAsync(TokenType.Bot, botToken);
-			await client.StartAsync();
-
-			return client;
+			Console.WriteLine("Disconnected.");
 		}
-    }
+	}
 }
