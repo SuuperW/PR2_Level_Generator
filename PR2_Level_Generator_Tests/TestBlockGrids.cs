1963878d4e640694d4998234e209e23f513b887b
Suuper <SuuperW@gmail.com>
2017-12-10 09:29:56 -0600

    Rename TestFastBlockGrid to TestBlockGrids and include testing LowMemoryBlockGrid, and move map tests to TestMapLE. Also fix tests for loading blank levels.

--- PR2_Level_Generator_Tests/TestBlockGrids.cs
+++ PR2_Level_Generator_Tests/TestBlockGrids.cs
@@ -5,13 +5,15 @@ using PR2_Level_Generator;
 
 namespace PR2_Level_Generator_Tests
 {
-	public class TestFastBlockGrid
+	public class TestBlockGrids
 	{
-		[Fact]
-		public void TestPlaceAndGet()
+		[Theory]
+		[InlineData(typeof(FastBlockGrid))]
+		[InlineData(typeof(LowMemoryBlockGrid))]
+		public void TestPlaceAndGet(Type gridType)
 		{
 			// Place the blocks
-			FastBlockGrid grid = new FastBlockGrid();
+			IBlockGrid grid = Activator.CreateInstance(gridType) as IBlockGrid;
 			grid.PlaceBlock(2, 5, 0);
 			grid.PlaceBlock(2, 8, 1);
 			grid.PlaceBlock(2, -5, 2);
@@ -29,11 +31,13 @@ namespace PR2_Level_Generator_Tests
 			Assert.Equal(BlockID.BLANK, grid.GetBlock(-24, 60).T);
 		}
 
-		[Fact]
-		public void TestDelete()
+		[Theory]
+		[InlineData(typeof(FastBlockGrid))]
+		[InlineData(typeof(LowMemoryBlockGrid))]
+		public void TestDelete(Type gridType)
 		{
 			// Place some blocks
-			FastBlockGrid grid = new FastBlockGrid();
+			IBlockGrid grid = Activator.CreateInstance(gridType) as IBlockGrid;
 			grid.PlaceBlock(2, 5, 0);
 			grid.PlaceBlock(2, 8, 1);
 			grid.PlaceBlock(4, 0, 2);
@@ -45,39 +49,6 @@ namespace PR2_Level_Generator_Tests
 			Assert.Equal(BlockID.BLANK, grid.GetBlock(2, 5).T);
 			grid.DeleteBlock(4, 0);
 			Assert.Equal(BlockID.BLANK, grid.GetBlock(4, 0).T);
-		}
-
-		[Fact]
-		public void TestMapLoad()
-		{
-			MapLE map = new MapLE(typeof(FastBlockGrid));
-
-			// Load some blocks
-			map.LoadLevel("data=m3`0`2;5,0;3;1,2;-8;2```````-1&title=testing");
-			// Assert those blocks exist
-			Assert.Equal(0, map.GetBlock(2, 5).T);
-			Assert.Equal(1, map.GetBlock(2, 8).T);
-			Assert.Equal(2, map.GetBlock(4, 0).T);
-
-			// Load a blank level
-			map.LoadLevel("");
-			// Assert that a block is placed at the origin
-			Assert.Equal(0, map.GetBlock(0, 0).T);
-		}
-
-		[Fact]
-		public void TestMapGetData()
-		{
-			MapLE map = new MapLE(typeof(FastBlockGrid));
-			// Place some blocks
-			map.AddBlock(2, 5, 0);
-			map.AddBlock(2, 8, 1);
-			map.AddBlock(4, 0, 2);
-
-			// Assert that GetData returns correct block data
-			string data = map.GetDataParam(false);
-			string blockData = data.Split('`')[2];
-			Assert.Equal("2;5,0;3;1,2;-8;2", blockData);
 		}
     }
 }
--- PR2_Level_Generator_Tests/TestMapLE.cs
+++ PR2_Level_Generator_Tests/TestMapLE.cs
@@ -32,7 +32,7 @@ namespace PR2_Level_Generator_Tests
 			Assert.Equal(0, map.MinimumRank);
 			Assert.Equal(0.0, map.Gravity);
 			Assert.Equal(0, map.TimeLimit);
-			Assert.Equal(0, map.CowboyChance);
+			Assert.Equal(5, map.CowboyChance);
 
 			Assert.Empty(map.avItems);
 			Assert.False(map.HasPassword);
@@ -42,6 +42,39 @@ namespace PR2_Level_Generator_Tests
 
 			Assert.Equal("", map.GetSetting("title"));
 			Assert.Equal("", map.GetSetting("credits"));
+		}
+
+		[Fact]
+		public void TestMapLoad()
+		{
+			MapLE map = new MapLE();
+
+			// Load some blocks
+			map.LoadLevel("data=m3`0`2;5,0;3;1,2;-8;2```````-1&title=testing");
+			// Assert those blocks exist
+			Assert.Equal(0, map.GetBlock(2, 5).T);
+			Assert.Equal(1, map.GetBlock(2, 8).T);
+			Assert.Equal(2, map.GetBlock(4, 0).T);
+
+			// Load a blank level
+			map.LoadLevel("");
+			// Assert that a block is placed at the origin
+			Assert.Equal(0, map.GetBlock(0, 0).T);
+		}
+
+		[Fact]
+		public void TestMapGetData()
+		{
+			MapLE map = new MapLE();
+			// Place some blocks
+			map.AddBlock(2, 5, 0);
+			map.AddBlock(2, 8, 1);
+			map.AddBlock(4, 0, 2);
+
+			// Assert that GetData returns correct block data
+			string data = map.GetDataParam(false);
+			string blockData = data.Split('`')[2];
+			Assert.Equal("2;5,0;3;1,2;-8;2", blockData);
 		}
 	}
 }
