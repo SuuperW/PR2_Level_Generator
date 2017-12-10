915b0739c9964fd895f5307c1810d5811e990a3c
Suuper <SuuperW@gmail.com>
2017-12-10 10:38:29 -0600

    Update TestBlockGrids.TestDelete to check more cases

--- PR2_Level_Generator_Tests/TestBlockGrids.cs
+++ PR2_Level_Generator_Tests/TestBlockGrids.cs
@@ -42,13 +42,31 @@ namespace PR2_Level_Generator_Tests
 			grid.PlaceBlock(2, 8, 1);
 			grid.PlaceBlock(4, 0, 2);
 
-			// Delete and then assert that those spaces are blank
+			// delete and assert that those spaces are now blank
 			grid.DeleteBlock(2, 8);
 			Assert.Equal(BlockID.BLANK, grid.GetBlock(2, 8).T);
-			grid.DeleteBlock(2, 5);
-			Assert.Equal(BlockID.BLANK, grid.GetBlock(2, 5).T);
 			grid.DeleteBlock(4, 0);
 			Assert.Equal(BlockID.BLANK, grid.GetBlock(4, 0).T);
+
+			grid.DeleteBlock(2, 6); // space is already blank; test that 'deleting' it won't delete other block
+			Assert.Equal(0, grid.GetBlock(2, 5).T);
+
+			// delete last block
+			grid.DeleteBlock(2, 5);
+			Assert.Equal(BlockID.BLANK, grid.GetBlock(2, 5).T);
+			Assert.Null(grid.FirstBlock);
+
+			// Ensure deletions haven't messed up block order
+			Block first = grid.PlaceBlock(6, 6, 0);
+			grid.PlaceBlock(7, 7, 0);
+			Block last = grid.PlaceBlock(8, 8, 0);
+
+			grid.DeleteBlock(7, 7);
+			Assert.Equal(first, grid.GetBlock(6, 6));
+			Assert.Equal(last, grid.GetBlock(8, 8));
+
+			Assert.Equal(first.next, last);
+			Assert.Equal(last.previous, first);
 		}
     }
 }
